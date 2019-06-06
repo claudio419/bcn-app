d735b865dc66ffd09ae9b87afa50013a9ab613e4
Bilel ayech <bilel.ayech@check24.de>
2019-06-06 16:45:26 +0200

    [PVRLV-7720]adding airplane photo and changing css

--- views/flight_bcn_details.php
+++ views/flight_bcn_details.php
@@ -3,7 +3,7 @@
 
     <h1 class="color_green_deep padding_top_15">Anreise</h1>
     <p>Hier findest du Infos zu deiner Anreise</p>
-
+    <div class="container_background container_background_shadow">
     <?php if($city_id > 1)  {?>
 
         <p class="padding_top_30">In Düsseldorf</p>
@@ -14,16 +14,22 @@
 
     <?php } ?>
 
-    <p class="padding_top_30">In München</p>
+    <p class="padding_top_30 ">In München</p>
 
     <div class="separator_gray"></div>
-    <p class="padding_top_15">Flug: <b>Lufthansa LH1812</b></p>
+    <p class="padding_top_15 ">Flug: <b>Lufthansa LH1812</b></p>
     <p>Wann: <b>12.07.2019 12:50 - 14:50</b></p>
 
     <p class="padding_top_30">In barcelona</p>
     <div class="separator_gray"></div>
     <p class="padding_top_15">Hotel Shuttle: <b>12.07.2019 15:00</b></p>
     <h4 class="center color_green font_weight_bold padding_top_30">Ankunft im Hotel ca. 16:00</h4>
-    <img src="public/img/flight_bcn.jpg" class="width_img_100 border_radius_10">
+    <h4 class="info_box info_text ">Wir fliegen mit Handgepäck. Kontrolliert, ob euer
+        Gepäckstück die richtigen Maße hat und vergesst nicht das ihr nur bestimmte Größen an Shampoo usw.
+        mitnehmen dürft. Genauere Infos findet ihr
+        <a class="info_link" href="https://hauptstadtkoffer.de/de/tipps-trips/gepaeckguide/airline/lufthansa"
+           target="_blank"> hier</a> bei der Lufthansa.</h4>
+
+    </div>
 
 </div>
