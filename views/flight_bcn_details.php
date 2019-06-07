ed34ecb582af94be7a9b49c95d997df7e211a160
Bilel ayech <bilel.ayech@check24.de>
2019-06-07 17:32:37 +0200

    [PVRLV-7720]Adding color and fixing info box

--- views/flight_bcn_details.php
+++ views/flight_bcn_details.php
@@ -7,9 +7,9 @@
         <div class="container_background_overlight border_radius_10">
             <?php if($city_id > 1)  {?>
 
-                <p class= "color_white">In Düsseldorf</p>
+                <p class= "color_white ">In Düsseldorf</p>
 
-                <div class="separator_gray"></div>
+                <div class="separator_gray padding_top_8"></div>
                 <p class="padding_top_10  color_white">Flug: <b>Lufthansa LH1812</b></p>
                 <p class="padding_top_5  color_white">Wann: <b>Düsseldorf nach München: <br>10:25 – 11:30 Uhr (LH2007)</br></b></p>
 
@@ -17,19 +17,19 @@
 
             <p class="padding_top_30  color_white">In München</p>
 
-            <div class="separator_gray"></div>
+            <div class="separator_gray padding_top_8"></div>
             <p class="padding_top_10  color_white">Flug: <b>Lufthansa LH1812</b></p>
             <p class="color_white padding_top_5">Wann: <b> München nach Barcelona: <br>12.07.2019 12:50 - 14:50 Uhr (LH1812)</br></b></p>
 
-            <p class="padding_top_30  color_white">In barcelona</p>
-            <div class="separator_gray"></div>
+            <p class="padding_top_30  color_white ">In Barcelona</p>
+            <div class="separator_gray padding_top_8"></div>
             <p class="padding_top_15  color_white">Hotel Shuttle: <b>12.07.2019 15:00</b></p>
             <h4 class="center color_green_apple font_weight_bold padding_top_30">Ankunft im Hotel ca. 16:00</h4>
 
-            <div class="inline_flex">
+            <div class="inline_flex  info_box_overlight border_radius_10">
                 <div class="info">info</div>
 
-                    <p class="info_box info_box_overlight info_text">Wir fliegen mit Handgepäck. Kontrolliert, ob euer
+                    <p class="info_box info_text">Wir fliegen mit Handgepäck. Kontrolliert, ob euer
                         Gepäckstück die richtigen Maße hat und vergesst nicht das ihr nur bestimmte Größen an Shampoo usw.
                         mitnehmen dürft. Genauere Infos findet ihr
                         <a class="info_link" href="https://hauptstadtkoffer.de/de/tipps-trips/gepaeckguide/airline/lufthansa"
--- views/flight_muc_details.php
+++ views/flight_muc_details.php
@@ -3,15 +3,15 @@
     <p>Hier findest du Infos zu deiner Abreise</p>
     <div class="margin_top_15 container_background border_radius_10">
         <div class="container_background_overlight border_radius_10">
-    <p class="padding_top_30 color_white">In barcelona</p>
-    <div class="separator_gray"></div>
+    <p class="padding_top_30 color_white">In Barcelona</p>
+    <div class="separator_gray padding_top_5"></div>
     <p class="padding_top_15 color_white">Hotel Shuttle: <b>14.07.2019 13:15</b></p>
     <p class="padding_top_5 color_white">Wann: <b>Barcelona nach München: <br>15:45 – 17:45 Uhr (LH1813)</br></b></p>
 
     <?php if($city_id > 1)  {?>
 
         <p class="padding_top_30 color_white">In München</p>
-        <div class="separator_gray"></div>
+        <div class="separator_gray padding_top_5"></div>
         <p class="padding_top_5 color_white">Wann: <b>München nach Düsseldorf: <br>18:30 – 19:40 Uhr (LH2734)</br></b></p>
         <h4 class="center color_green_apple font_weight_bold padding_top_30">Ankunft in Düsseldorf 19:40</h4>
 
