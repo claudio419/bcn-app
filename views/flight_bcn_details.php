1165b9a99b733d583dc65394495713c8fc35723b
Bilel ayech <bilel.ayech@check24.de>
2019-06-07 11:50:18 +0200

    [PVRLV-7720] adding css

--- views/flight_bcn_details.php
+++ views/flight_bcn_details.php
@@ -1,35 +1,47 @@
 
 <div class="container">
 
-    <h1 class="color_green_deep padding_top_15">Anreise</h1>
+    <h1 class="color_green_deep padding_top_15 color_white">Anreise</h1>
     <p>Hier findest du Infos zu deiner Anreise</p>
-    <div class="container_background container_background_shadow">
-    <?php if($city_id > 1)  {?>
+    <div class="margin_top_15 container_background border_radius_10">
+        <div class="container_background_overlight border_radius_10">
+            <?php if($city_id > 1)  {?>
 
-        <p class="padding_top_30">In Düsseldorf</p>
+                <p class= "color_white">In Düsseldorf</p>
 
-        <div class="separator_gray"></div>
-        <p class="padding_top_10">Flug: <b>Lufthansa LH1812</b></p>
-        <p class="padding_top_5">Wann: <b>Düsseldorf nach München: 10:25 – 11:30 Uhr (LH2007)</b></p>
+                <div class="separator_gray"></div>
+                <p class="padding_top_10  color_white">Flug: <b>Lufthansa LH1812</b></p>
+                <p class="padding_top_5  color_white">Wann: <b>Düsseldorf nach München: 10:25 – 11:30 Uhr (LH2007)</b></p>
 
-    <?php } ?>
+            <?php } ?>
 
-    <p class="padding_top_30 ">In München</p>
+            <p class="padding_top_30  color_white">In München</p>
 
-    <div class="separator_gray"></div>
-    <p class="padding_top_15 ">Flug: <b>Lufthansa LH1812</b></p>
-    <p>Wann: <b>12.07.2019 12:50 - 14:50</b></p>
+            <div class="separator_gray"></div>
+            <p class="padding_top_15  color_white">Flug: <b>Lufthansa LH1812</b></p>
+            <p>Wann: <b>12.07.2019 12:50 - 14:50</b></p>
 
-    <p class="padding_top_30">In barcelona</p>
-    <div class="separator_gray"></div>
-    <p class="padding_top_15">Hotel Shuttle: <b>12.07.2019 15:00</b></p>
-    <h4 class="center color_green font_weight_bold padding_top_30">Ankunft im Hotel ca. 16:00</h4>
-    <h4 class="info_box info_text ">Wir fliegen mit Handgepäck. Kontrolliert, ob euer
-        Gepäckstück die richtigen Maße hat und vergesst nicht das ihr nur bestimmte Größen an Shampoo usw.
-        mitnehmen dürft. Genauere Infos findet ihr
-        <a class="info_link" href="https://hauptstadtkoffer.de/de/tipps-trips/gepaeckguide/airline/lufthansa"
-           target="_blank"> hier</a> bei der Lufthansa.</h4>
+            <p class="padding_top_30  color_white">In barcelona</p>
+            <div class="separator_gray"></div>
+            <p class="padding_top_15  color_white">Hotel Shuttle: <b>12.07.2019 15:00</b></p>
+            <h4 class="center color_green_apple font_weight_bold padding_top_30">Ankunft im Hotel ca. 16:00</h4>
 
+            <div class="center inline_flex">
+                <div >info</div>
+                <div>
+                    <p class="info_box info_text ">Wir fliegen mit Handgepäck. Kontrolliert, ob euer
+                        Gepäckstück die richtigen Maße hat und vergesst nicht das ihr nur bestimmte Größen an Shampoo usw.
+                        mitnehmen dürft. Genauere Infos findet ihr
+                        <a class="info_link" href="https://hauptstadtkoffer.de/de/tipps-trips/gepaeckguide/airline/lufthansa"
+                           target="_blank"> hier</a> bei der Lufthansa.</p>
+                </div>
+
+            </div>
+
+        </div>
     </div>
+
+
+
 
 </div>
