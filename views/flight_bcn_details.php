90b5db915fb21b9c016fe05c1496c5300d37c245
claudio <claudio.roman@check24.de>
2019-06-11 13:28:29 +0200

    [PVRLV-7720]git ignore was added

--- views/flight_bcn_details.php
+++ views/flight_bcn_details.php
@@ -9,7 +9,7 @@
 
                 <p class= "color_white ">In Düsseldorf</p>
 
-                <div class="separator_gray padding_top_8"></div>
+                <div class="separator_gray padding_top_10"></div>
                 <p class="padding_top_10  color_white">Flug: <b>Lufthansa LH1812</b></p>
                 <p class="padding_top_5  color_white">Wann: <b>Düsseldorf nach München: <br>10:25 – 11:30 Uhr (LH2007)</br></b></p>
 
@@ -17,14 +17,14 @@
 
             <p class="padding_top_30  color_white">In München</p>
 
-            <div class="separator_gray padding_top_8"></div>
+            <div class="separator_gray padding_top_10"></div>
             <p class="padding_top_10  color_white">Flug: <b>Lufthansa LH1812</b></p>
             <p class="color_white padding_top_5">Wann: <b> München nach Barcelona: <br>12.07.2019 12:50 - 14:50 Uhr (LH1812)</br></b></p>
 
             <p class="padding_top_30  color_white ">In Barcelona</p>
-            <div class="separator_gray padding_top_8"></div>
+            <div class="separator_gray padding_top_10"></div>
             <p class="padding_top_15  color_white">Hotel Shuttle: <b>12.07.2019 15:00</b></p>
-            <h4 class="center color_green_apple font_weight_bold padding_top_30 padding_bottom_79">Ankunft im Hotel ca. 16:00</h4>
+            <h4 class="center color_green_apple font_weight_bold padding_top_30 padding_bottom_50">Ankunft im Hotel ca. 16:00</h4>
 
             <div class="inline_flex  info_box_overlight border_radius_10">
                 <div class="info">info</div>
