07202545bc2b1f8a988da11913758ca2a0800c60
claudio <claudio.roman@check24.de>
2019-06-12 18:20:26 +0200

    [PVRLV-7720] p size was change and new pic added

--- views/flight_bcn_details.php
+++ views/flight_bcn_details.php
@@ -2,22 +2,25 @@
 <div class="container">
 
     <h1 class="color_green_deep padding_top_15 color_white">Anreise</h1>
-    <p>Hier findest du Infos zu deiner Anreise</p>
-    <div class="margin_top_15 container_background border_radius_10">
+    <p>Hier findest Du Infos zu Deiner Anreise</p>
+    <div class="margin_top_15 container_background_bcn border_radius_10">
         <div class="container_background_overlight border_radius_10">
             <?php if($city_id > 1)  {?>
 
-                <p class= "color_white ">In Düsseldorf</p>
-
-                <div class="separator_gray padding_top_10"></div>
+                <h4 class= "color_white ">In Düsseldorf</h4>
+                <div class="separator_gray"></div>
                 <p class="padding_top_10  color_white">Flug: <b>Lufthansa LH1812</b></p>
                 <p class="padding_top_5  color_white">Wann: <b>Düsseldorf nach München: <br>10:25 – 11:30 Uhr (LH2007)</br></b></p>
 
+                <h4 class="padding_top_30  color_white">In München</h4>
+
             <?php } ?>
 
-            <p class="padding_top_30  color_white">In München</p>
+            <?php if($city_id == 1)  {?>
+                <h4 class="padding_top_15  color_white">In München</h4>
+            <?php } ?>
 
-            <div class="separator_gray padding_top_10"></div>
+            <div class="separator_gray"></div>
             <p class="padding_top_10  color_white">Flug: <b>Lufthansa LH1812</b></p>
             <p class="color_white padding_top_5">Wann: <b> München nach Barcelona: <br>12.07.2019 12:50 - 14:50 Uhr (LH1812)</br></b></p>
 
@@ -33,7 +36,7 @@
                         Gepäckstück die richtigen Maße hat und vergesst nicht das ihr nur bestimmte Größen an Shampoo usw.
                         mitnehmen dürft. Genauere Infos findet ihr
                         <a class="info_link" href="https://hauptstadtkoffer.de/de/tipps-trips/gepaeckguide/airline/lufthansa"
-                           target="_blank"> hier</a> bei der Lufthansa.</p>
+                           target="_blank"> hier</a></p>
             </div>
 
         </div>
--- views/flight_muc_details.php
+++ views/flight_muc_details.php
@@ -1,9 +1,9 @@
 <div class="container">
     <h1 class="color_green_deep padding_top_15">Abreise</h1>
     <p>Hier findest du Infos zu deiner Abreise</p>
-    <div class="margin_top_15 container_background border_radius_10">
+    <div class="margin_top_15 container_background_muc border_radius_10">
         <div class="container_background_overlight border_radius_10">
-    <p class="padding_top_30 color_white">In Barcelona</p>
+    <p class="padding_top_15 color_white">In Barcelona</p>
     <div class="separator_gray padding_top_5"></div>
     <p class="padding_top_15 color_white">Hotel Shuttle: <b>14.07.2019 13:15</b></p>
     <p class="padding_top_5 color_white">Wann: <b>Barcelona nach München: <br>15:45 – 17:45 Uhr (LH1813)</br></b></p>
