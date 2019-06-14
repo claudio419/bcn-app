0bd39910eab5d6a408cd0d54e101be6735ccc57d
Bilel ayech <bilel.ayech@check24.de>
2019-06-14 18:17:14 +0200

    [PVRLV-7593]Design improvement

--- views/hotel_content.php
+++ views/hotel_content.php
@@ -5,31 +5,64 @@
     </p>
     <div class="margin_top_15 container_background_hotel border_radius_10">
         <div class="container_background_overlight_hotel border_radius_10">
-            <p class="padding_top_15 color_white">Name:
-                <b>
-                    <a href="https://www.motel-one.com/de/hotels/barcelona/hotel-barcelona-ciutadella/">Hotel Motel One Barcelona-Ciutadella</a>
-                </b>
-            </p>
-            <p class="padding_top_15 color_white">Wo: <a href="https://goo.gl/maps/WM7SRNYx7LakaV5B8"> Passeig de Pujades, 11-13, 08018  Barcelona</a></p>
-            <p class="padding_top_15 color_white">Frühstück: <b>Täglich wird von 7:00 - 11:00 Uhr ein Frühstücksbuffet angeboten.</b></p>
-            <p class="padding_top_15 color_white">Info: <b>Im gesamten Hotel gibt es kostenloses WLAN.</b></p>
-            <p class="padding_top_30 color_white">Umgebung:
-            <div class="link_hotel_padding">
-                <a href="https://www.barcelona.de/de/barcelona-placa-catalunya.html"><span class="numberCircle">1</span> Plaça de Catalunya (1,4 km)</a><br>
-                <a href="https://www.barcelona.de/de/barcelona-casa-mila-pedrera.html" ><span class="numberCircle">2</span> Casa Milà - La Pedrera (2,1 km) </a><br>
-                <a href="https://www.barcelona.de/de/barcelona-casa-batllo.html "><span class="numberCircle">3</span> Casa Batlló (1,6 km) </a><br>
-                <a href="https://www.barcelona.de/de/barcelona-rambla.html"><span class="numberCircle">4</span> Las Ramblas (1,5 km) </a><br>
-                <a href="https://www.barcelona.de/de/barcelona-sagrada-familia.html "><span class="numberCircle">5</span> Sagrada Familia (1,8 km)</a><br>
-                <a href="https://www.barcelona.de/de/barcelona-parc-ciutadella.html"><span class="numberCircle">6</span> Parc Ciutadella - Zoo (0,2 km) </a><br>
-                <a href="https://www.barcelona.de/de/barcelona-park-guell.html "><span class="numberCircle">7</span> Parc Güell (3,9 km)</a><br>
-                <a href="https://www.barcelona.de/de/barcelona-gotisches-viertel-barri-gotic.html"><span class="numberCircle">8</span> Barrio Gotico (1,6 km) </a><br>
-                <a href="https://www.barcelona.de/de/barcelona-strand.html"><span class="numberCircle">9</span> Platja Barceloneta (1,0 km)</a><br>
-                <a href="https://www.barcelona.de/de/barcelona-museum-picasso.html"><span class="numberCircle">10</span> Museu Picasso (0,7 km)</a><br>
-                <a href="https://www.barcelona.de/de/fc-barcelona-camp-nou-stadion.html"><span class="numberCircle">11</span> Camp Nou (6,5 km)</a><br>
-                <a href="https://www.barcelona.de/de/barcelona-stadtteil-montjuic.html"><span class="numberCircle">12</span> Park Montjuic (4,8 km)</a><br>
-                <a href="https://www.barcelona.de/de/mercat-boqueria-sant-josep-barcelona.html"><span class="numberCircle">13</span> La Bocceria (1,7 km)</a><br>
+
+            <div class="inline_flex padding_top_10 color_white">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Name: </p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white"><a href="https://www.motel-one.com/de/hotels/barcelona/hotel-barcelona-ciutadella/">Hotel Motel One Barcelona-Ciutadella</a></p>
+                </div>
             </div>
-            </p>
+
+            <div class="inline_flex padding_top_10 color_white">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Wo: </p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white"><a href="https://goo.gl/maps/WM7SRNYx7LakaV5B8"> Passeig de Pujades, 11-13, 08018  Barcelona</a></p>
+                </div>
+            </div>
+
+
+            <div class="inline_flex padding_top_10 color_white">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Frühstück: </p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white">Täglich wird von 7:00 - 11:00 Uhr ein Frühstücksbuffet angeboten.</p>
+                </div>
+            </div>
+
+            <div class="inline_flex padding_top_10 color_white">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">info:</p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white">Im gesamten Hotel gibt es kostenloses WLAN</p>
+                </div>
+            </div>
+
+           <div class="inline_flex padding_top_30">
+               <div class="left">
+                   <p class="color_white">Umgebung:</p>
+               </div>
+               <div class="right">
+                   <a href="https://www.barcelona.de/de/barcelona-placa-catalunya.html"><span class="numberCircle">1</span> Plaça de Catalunya (1,4 km)</a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-casa-mila-pedrera.html" ><span class="numberCircle">2</span> Casa Milà - La Pedrera (2,1 km) </a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-casa-batllo.html "><span class="numberCircle">3</span> Casa Batlló (1,6 km) </a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-rambla.html"><span class="numberCircle">4</span> Las Ramblas (1,5 km) </a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-sagrada-familia.html "><span class="numberCircle">5</span> Sagrada Familia (1,8 km)</a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-parc-ciutadella.html"><span class="numberCircle">6</span> Parc Ciutadella - Zoo (0,2 km) </a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-park-guell.html "><span class="numberCircle">7</span> Parc Güell (3,9 km)</a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-gotisches-viertel-barri-gotic.html"><span class="numberCircle">8</span> Barrio Gotico (1,6 km) </a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-strand.html"><span class="numberCircle">9</span> Platja Barceloneta (1,0 km)</a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-museum-picasso.html"><span class="numberCircle">10</span> Museu Picasso (0,7 km)</a><br>
+                   <a href="https://www.barcelona.de/de/fc-barcelona-camp-nou-stadion.html"><span class="numberCircle">11</span> Camp Nou (6,5 km)</a><br>
+                   <a href="https://www.barcelona.de/de/barcelona-stadtteil-montjuic.html"><span class="numberCircle">12</span> Park Montjuic (4,8 km)</a><br>
+                   <a href="https://www.barcelona.de/de/mercat-boqueria-sant-josep-barcelona.html"><span class="numberCircle">13</span> La Bocceria (1,7 km)</a><br>
+               </div>
+           </div>
         </div>
     </div>
 </div>
