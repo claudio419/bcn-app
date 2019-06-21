752119dfd224343a16eb6e877cb77ef402b6e3e0
Bilel ayech <bilel.ayech@check24.de>
2019-06-21 15:15:58 +0200

    [PVRLV-7593]Fixing position of umgebung list

--- views/hotel_content.php
+++ views/hotel_content.php
@@ -48,127 +48,127 @@
                    <p class="color_white">Umgebung:</p>
                </div>
                <div class="right">
-                   <div class="padding_top_10 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">1</span>
+                   <div class="inline_flex">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">1</span>
                        </div>
-                       <div class="width_80_percent">
-                           <a href="https://www.barcelona.de/de/barcelona-placa-catalunya.html"> Plaça de Catalunya (1,4 km)</a>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-placa-catalunya.html">Plaça de Catalunya (1,4 km)</a>
                        </div>
 
                    </div>
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">2</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">2</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-casa-mila-pedrera.html">Casa Milà - La Pedrera (2,1 km)</a>
                        </div>
                    </div>
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">3</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">3</span>
                        </div>
-                       <div class="width_80_percent_">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-casa-batllo.html "> Casa Batlló (1,6 km) </a>
                        </div>
 
                    </div>
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent ">
-                           <span class="numberCircle">4</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">4</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-rambla.html"> Las Ramblas (1,5 km) </a>
                        </div>
 
                    </div>
 
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">5</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">5</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-sagrada-familia.html "> Sagrada Familia (1,8 km)</a>
                        </div>
 
                    </div>
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">6</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">6</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a class="padding_top_30 padding_bottom_50" href="https://www.barcelona.de/de/barcelona-parc-ciutadella.html"> Parc Ciutadella - Zoo (0,2 km) </a>
                        </div>
 
                    </div>
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">7</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">7</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-park-guell.html "> Parc Güell (3,9 km)</a>
                        </div>
 
                    </div>
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">8</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">8</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-gotisches-viertel-barri-gotic.html"> Barrio Gotico (1,6 km) </a>
                        </div>
 
                    </div>
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">9</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">9</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-strand.html"> Platja Barceloneta (1,0 km)</a>
                        </div>
 
                    </div>
 
 
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">10</span></div>
-                       <div class="width_80_percent">
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">10</span></div>
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-museum-picasso.html"> Museu Picasso (0,7 km)</a>
                        </div>
 
                    </div>
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">11</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">11</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/fc-barcelona-camp-nou-stadion.html"> Camp Nou (6,5 km)</a>
                        </div>
 
                    </div>
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="">
-                           <span class="numberCircle">12</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">12</span>
                        </div>
-                       <div class="">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/barcelona-stadtteil-montjuic.html"> Park Montjuic (4,8 km)</a>
                        </div>
 
                    </div>
-                   <div class="padding_top_5 width_60_percent inline_flex">
-                       <div class="width_20_percent">
-                           <span class="numberCircle">13</span>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">13</span>
                        </div>
-                       <div class="width_80_percent">
+                       <div class="width_auto margin_left_5">
                            <a href="https://www.barcelona.de/de/mercat-boqueria-sant-josep-barcelona.html"> La Bocceria (1,7 km)</a>
                        </div>
 
