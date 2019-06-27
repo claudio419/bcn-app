9217acf41cbd02e27ecdcd8cc299d4350926c3b0
Bilel ayech <bilel.ayech@check24.de>
2019-06-27 13:01:55 +0200

    Adding Text in hotel and anreise page

---
+++ views/hotel_content.php
@@ -0,0 +1,197 @@
+<div class="container padding_top_5">
+    <h1 class="color_green_deep padding_top_15">Hotel</h1>
+    <p>
+        Hier findest du Infos zu dem Hotel und deinem Zimmer.
+    </p>
+    <div class="margin_top_15 container_background_hotel border_radius_10">
+        <div class="container_background_overlight_hotel border_radius_10">
+
+            <div class="inline_flex padding_top_10 color_white width_100">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Name: </p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white"><a href="https://www.motel-one.com/de/hotels/barcelona/hotel-barcelona-ciutadella/">Hotel Motel One Barcelona-Ciutadella</a></p>
+                </div>
+            </div>
+
+            <div class="inline_flex padding_top_10 color_white width_100">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Wo: </p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white"><a href="https://goo.gl/maps/WM7SRNYx7LakaV5B8"> Passeig de Pujades, 11-13, 08018  Barcelona</a></p>
+                </div>
+            </div>
+
+
+            <div class="inline_flex padding_top_10 color_white width_100">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Frühstück: </p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white">Täglich wird von 7:00 - 11:00 Uhr ein Frühstücksbuffet angeboten.</p>
+                </div>
+            </div>
+
+            <div class="inline_flex padding_top_10 color_white width_100">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Check-In:</p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white"> Ihr kommt am Freitag gemeinsam im Hotel an. Dort wirst Du von dem Orga - Team deine Zimmerkarte bekommen</p>
+                </div>
+            </div>
+            <div class="inline_flex padding_top_10 color_white width_100">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">Check-Out:</p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white">Die Info zum Check-Out bekommt ihr am Sonntag von dem Orga - Team</p>
+                </div>
+            </div>
+
+            <div class="inline_flex padding_top_10 color_white width_100">
+                <div class="left color_white width_30_percent">
+                    <p class="color_white">info:</p>
+                </div>
+                <div class="right width_60_percent">
+                    <p class="color_white">Im gesamten Hotel gibt es kostenloses WLAN</p>
+                </div>
+            </div>
+
+           <div class="inline_flex padding_top_30">
+               <div class="left">
+                   <p class="color_white">Umgebung:</p>
+               </div>
+               <div class="right">
+                   <div class="inline_flex">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">1</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-placa-catalunya.html">Plaça de Catalunya (1,4 km)</a>
+                       </div>
+
+                   </div>
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">2</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-casa-mila-pedrera.html">Casa Milà - La Pedrera (2,1 km)</a>
+                       </div>
+                   </div>
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">3</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-casa-batllo.html "> Casa Batlló (1,6 km) </a>
+                       </div>
+
+                   </div>
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">4</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-rambla.html"> Las Ramblas (1,5 km) </a>
+                       </div>
+
+                   </div>
+
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">5</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-sagrada-familia.html "> Sagrada Familia (1,8 km)</a>
+                       </div>
+
+                   </div>
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">6</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a class="padding_top_30 padding_bottom_50" href="https://www.barcelona.de/de/barcelona-parc-ciutadella.html"> Parc Ciutadella - Zoo (0,2 km) </a>
+                       </div>
+
+                   </div>
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">7</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-park-guell.html "> Parc Güell (3,9 km)</a>
+                       </div>
+
+                   </div>
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">8</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-gotisches-viertel-barri-gotic.html"> Barrio Gotico (1,6 km) </a>
+                       </div>
+
+                   </div>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">9</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-strand.html"> Platja Barceloneta (1,0 km)</a>
+                       </div>
+
+                   </div>
+
+
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">10</span></div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-museum-picasso.html"> Museu Picasso (0,7 km)</a>
+                       </div>
+
+                   </div>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">11</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/fc-barcelona-camp-nou-stadion.html"> Camp Nou (6,5 km)</a>
+                       </div>
+
+                   </div>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">12</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/barcelona-stadtteil-montjuic.html"> Park Montjuic (4,8 km)</a>
+                       </div>
+
+                   </div>
+                   <div class="inline_flex padding_top_10">
+                       <div class="width_auto">
+                           <span class="numberCircle margin_left_5">13</span>
+                       </div>
+                       <div class="width_auto margin_left_5">
+                           <a href="https://www.barcelona.de/de/mercat-boqueria-sant-josep-barcelona.html"> La Bocceria (1,7 km)</a>
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+        </div>
+    </div>
+</div>
