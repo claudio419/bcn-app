fb3e1eddb9c9448708a9c6aea2a1f0d432810834
claudio <claudio.roman@check24.de>
2019-07-03 11:04:38 +0200

    [PVRLV-7879] info text hide

--- views/flight_bcn_details.php
+++ views/flight_bcn_details.php
@@ -42,19 +42,19 @@
             <p class="padding_top_30  color_white ">In Barcelona</p>
             <div class="separator_gray padding_top_10"></div>
             <p class="padding_top_15  color_white">Hotel Shuttle: <b>12.07.2019 15:00</b></p>
-            <h4 class="center color_green_apple font_weight_bold padding_top_30 padding_bottom_50">Ankunft im Hotel ca. 16:00</h4>
+            <h4 class="center color_green_apple font_weight_bold padding_top_30">Ankunft im Hotel ca. 16:00</h4>
 
-            <div class="inline_flex  info_box_overlight border_radius_10">
-                <div class="info">info</div>
-
-
-                <p class="info_box info_text">Du kannst Dich bereits 23 Stunden vor Abflug hier online einchecken,
-                    Deinen Sitzplatz auswählen und Deine online Bordkarte ausdrucken oder sie aufs Mobiltelefon
-                    senden lassen. Dafür folgst Du einfach den Anweisungen bei diesem
-                    <a class="info_link" href="https://www.lufthansa.com/at/de/online-check-in" >Link</a> und gibst Deinen Namen
-                    und die Buchungsnummer (die Du auch per Mail bekommen hast) ein.
-                    </p>
-            </div>
+<!--            <div class="inline_flex  info_box_overlight border_radius_10">-->
+<!--                <div class="info">info</div>-->
+<!---->
+<!---->
+<!--                <p class="info_box info_text">Du kannst Dich bereits 23 Stunden vor Abflug hier online einchecken,-->
+<!--                    Deinen Sitzplatz auswählen und Deine online Bordkarte ausdrucken oder sie aufs Mobiltelefon-->
+<!--                    senden lassen. Dafür folgst Du einfach den Anweisungen bei diesem-->
+<!--                    <a class="info_link" href="https://www.lufthansa.com/at/de/online-check-in" >Link</a> und gibst Deinen Namen-->
+<!--                    und die Buchungsnummer (die Du auch per Mail bekommen hast) ein.-->
+<!--                    </p>-->
+<!--            </div>-->
 
 
         </div>
